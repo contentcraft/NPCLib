4a035db40fea1e7d1e20faf7c2b83238173145e9
Mats <mats@contentcraftmc.nl>
2022-04-14 14:30:38 +0200

    Automatic provision of everything

--- api/src/main/java/net/jitse/npclib/impl/ImplementedNpc.java
+++ api/src/main/java/net/jitse/npclib/impl/ImplementedNpc.java
@@ -97,7 +97,7 @@ public class ImplementedNpc extends NPCBase {
 
         // Removing the player info after 10 seconds.
         Bukkit.getScheduler().runTaskLater(instance.getPlugin(), () ->
-                playerConnection.sendPacket(packetPlayOutPlayerInfoRemove), 20 * 5);
+                playerConnection.sendPacket(packetPlayOutPlayerInfoRemove), 20 * 10);
     }
 
     @Override
