4ba9d4326b50805fd06b68c307b4efc38c4119df
Onno <emailodg@gmail.com>
2022-04-24 12:26:14 +0200

    Unfucked Lobby NPC dissappearance :D. Closes contentcraft/lobby#18

--- api/src/main/java/net/jitse/npclib/listeners/HandleMoveBase.java
+++ api/src/main/java/net/jitse/npclib/listeners/HandleMoveBase.java
@@ -1,5 +1,6 @@
 package net.jitse.npclib.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import net.jitse.npclib.internal.NPCBase;
@@ -9,9 +10,14 @@ public class HandleMoveBase {
 
     void handleMove(Player player) {
         for (NPCBase npc : NPCManager.getAllNPCs()) {
+            //Ik begrijp niet helemaal wat deze statement intentionally zou moeten doen.
+            //Momenteel prevent hij in ieder geval dat NPC's niet meer visible worden als ze eenmaal unshown zijn, dus heb ik het gecomment.
+            //xx Onno
+            /*
             if (!npc.getShown().contains(player.getUniqueId())) {
                 continue; // NPC was never supposed to be shown to the player.
             }
+            */
 
             if (!npc.isShown(player) && npc.inRangeOf(player) && npc.inViewOf(player)) {
                 // The player is in range and can see the NPC, auto-show it.
